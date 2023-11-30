8dd19139f58fec76bd8daccc2c6682ac27e16a47
valentin994 <valentin.vareskic@gmail.com>
2023-11-30 16:48:00 +0100

    added parsing arguments from the cli

---
+++ README.md
@@ -0,0 +1,3 @@
+# Todominal
+
+The goal of this project is to create a todo application for the terminal.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use::clap::{Parser, ValueEnum};
+
+//TODO Add a way to add a todo
+//TODO A way to remove the todo
+//TODO A way to check on which operating system I am and how to store the todo
+//TODO A way to reorganize the todos
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(value_enum)]
+    operation: Operation,
+    #[arg(short, long)]
+    text: String,
+}
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Operation {
+    Add,
+    Remove,
+    Modify
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("{:?}", args)
+
 }
