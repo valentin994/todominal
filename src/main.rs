5c2e7db29b29d333a6f9b8580f70400299fd2411
valentin994 <valentin.vareskic@gmail.com>
2023-12-01 13:38:38 +0100

    implemented listing todos

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use::clap::{Parser, ValueEnum};
-use rusqlite::{Connection, Result, Error};
+use ::clap::{Parser, ValueEnum};
+use rusqlite::{Connection, Error, Result};
+use std::fmt::Display;
+use std::fmt;
 
-//TODO Add a way to add a todo
+//TODO Add a way to fetch all todos
+//TODO Change code to use Subcommands
 //TODO A way to remove the todo
 //TODO A way to check on which operating system I am and how to store the todo
 //TODO A way to reorganize the todos
@@ -13,7 +16,7 @@ use rusqlite::{Connection, Result, Error};
 struct Args {
     #[arg(value_enum)]
     operation: Operation,
-    #[arg(short, long)]
+    #[arg(short, long, default_value = " ")]
     text: String,
     #[arg(short, long, default_value = "medium")]
     priority: String,
@@ -22,14 +25,15 @@ struct Args {
 enum Operation {
     Add,
     Remove,
-    Modify
+    Modify,
+    Ls,
 }
 
 fn create_table() -> Result<(Connection), Error> {
     let path = "./my_db.db3";
     let db = Connection::open(path)?;
     // Use the database somehow...
-   /* db.execute(
+    /* db.execute(
         "CREATE TABLE todo (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             todo_text  TEXT NOT NULL,
@@ -41,34 +45,72 @@ fn create_table() -> Result<(Connection), Error> {
     Ok(db)
 }
 
-fn insert_todo(conn: Connection, todo_text: String, priority: String) -> Result<(), Error>{
-    conn.execute("INSERT into todo (todo_text, priority) VALUES (?1, ?2)", (todo_text, priority))?;
+fn insert_todo(conn: Connection, todo_text: String, priority: String) -> Result<(), Error> {
+    conn.execute(
+        "INSERT into todo (todo_text, priority) VALUES (?1, ?2)",
+        (todo_text, priority),
+    )?;
     Ok(())
 }
 
 fn remove_todo(conn: Connection, todo_text: String, priority: String) -> Result<(), Error> {
-    conn.execute("INSERT into todo (todo_text, priority) VALUES (?1, ?2)", (todo_text, priority))?;
+    conn.execute(
+        "INSERT into todo (todo_text, priority) VALUES (?1, ?2)",
+        (todo_text, priority),
+    )?;
     Ok(())
 }
 
 fn modify_todo(conn: Connection, todo_text: String, priority: String) -> Result<(), Error> {
-    conn.execute("INSERT into todo (todo_text, priority) VALUES (?1, ?2)", (todo_text, priority))?;
+    conn.execute(
+        "INSERT into todo (todo_text, priority) VALUES (?1, ?2)",
+        (todo_text, priority),
+    )?;
     Ok(())
+}
+
+fn get_all_todos(conn: Connection) -> Result<(), Error> {
+    let mut statement = conn.prepare("SELECT * FROM todo")?;
+    let todo_iter = statement.query_map([], |row| {
+        Ok(Todo {
+            id: row.get(0)?,
+            todo_text: row.get(1)?,
+            priority: row.get(2)?,
+        })
+    })?;
+    for todo in todo_iter {
+        println!("{}", todo.unwrap());
+    }
+    Ok(())
+}
+#[derive(Debug)]
+struct Todo {
+    id: u16,
+    todo_text: String,
+    priority: String,
+}
+
+impl Display for Todo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}. {} priority: {}",self.id,self.todo_text,self.priority)
+    }
 }
 
 fn main() {
     let conn = create_table().expect("Failed to connect to DB");
     let args = Args::parse();
     match args.operation {
-        add => {
+        Operation::Add => {
             insert_todo(conn, args.text, args.priority);
-        },
-        remove => {
+        }
+        Operation::Remove => {
             remove_todo(conn, args.text, args.priority);
-        },
-        modify => {
-            modify_todo(conn,args.text, args.priority);
-        },
+        }
+        Operation::Modify => {
+            modify_todo(conn, args.text, args.priority);
+        }
+        Operation::Ls => {
+            get_all_todos(conn);
+        }
     }
-
 }
