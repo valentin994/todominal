587772a921d8f17b0a28f265b0564006cd185357
valentin994 <valentin.vareskic@gmail.com>
2023-12-04 17:17:25 +0100

    separated db operations into crud file

---
+++ src/crud.rs
@@ -0,0 +1,53 @@
+use rusqlite::{Connection, Error, Result};
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(Debug)]
+struct Todo {
+    id: u16,
+    todo_text: String,
+    priority: String,
+}
+
+impl Display for Todo {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "○ {} \t\tID: {} \n  ->priority: {}\n",
+            self.todo_text, self.id, self.priority
+        )
+    }
+}
+
+pub fn insert_todo(conn: Connection, todo_text: String, priority: String) -> Result<(), Error> {
+    conn.execute(
+        "INSERT into todo (todo_text, priority) VALUES (?1, ?2)",
+        (todo_text, priority),
+    )?;
+    Ok(())
+}
+
+pub fn remove_todo(conn: Connection, id: i32) -> Result<(), Error> {
+    conn.execute("DELETE from todo where id = ?", [&id])?;
+    Ok(())
+}
+
+pub fn modify_todo(conn: Connection) -> Result<(), Error> {
+    conn.prepare("DELETE from todo where id = VALUES (?1)")?;
+    Ok(())
+}
+
+pub fn get_all_todos(conn: Connection) -> Result<(), Error> {
+    let mut statement = conn.prepare("SELECT * FROM todo")?;
+    let todo_iter = statement.query_map([], |row| {
+        Ok(Todo {
+            id: row.get(0)?,
+            todo_text: row.get(1)?,
+            priority: row.get(2)?,
+        })
+    })?;
+    for todo in todo_iter {
+        println!("{}", todo.unwrap());
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use ::clap::{Parser, ValueEnum};
-use rusqlite::{params, Connection, Error, Result};
-use std::fmt;
-use std::fmt::Display;
+mod crud;
+use crud::{get_all_todos, insert_todo, modify_todo, remove_todo};
 
+use ::clap::{Parser, ValueEnum};
+use rusqlite::{Connection, Error, Result};
+
+//TODO Cleanup and file separation
 //TODO A way to update the todo
 //TODO Change code to use Subcommands
 //TODO A way to check on which operating system I am and how to store the todo
@@ -31,7 +33,7 @@ enum Operation {
     Ls,
 }
 
-fn create_table() -> Result<(Connection), Error> {
+fn create_table() -> Result<Connection, Error> {
     let path = "./my_db.db3";
     let db = Connection::open(path)?;
     // Use the database somehow...
@@ -47,61 +49,15 @@ fn create_table() -> Result<(Connection), Error> {
     Ok(db)
 }
 
-fn insert_todo(conn: Connection, todo_text: String, priority: String) -> Result<(), Error> {
-    conn.execute(
-        "INSERT into todo (todo_text, priority) VALUES (?1, ?2)",
-        (todo_text, priority),
-    )?;
-    Ok(())
-}
-
-fn remove_todo(conn: Connection, id: i32) -> Result<(), Error> {
-    conn.execute("DELETE from todo where id = ?", ([&id]))?;
-    Ok(())
-}
-
-fn modify_todo(conn: Connection, todo_text: String, priority: String) -> Result<(), Error> {
-    let mut stms = conn.prepare("DELETE from todo where id = VALUES (?1)")?;
-    Ok(())
-}
-
-fn get_all_todos(conn: Connection) -> Result<(), Error> {
-    let mut statement = conn.prepare("SELECT * FROM todo")?;
-    let todo_iter = statement.query_map([], |row| {
-        Ok(Todo {
-            id: row.get(0)?,
-            todo_text: row.get(1)?,
-            priority: row.get(2)?,
-        })
-    })?;
-    for todo in todo_iter {
-        println!("{}", todo.unwrap());
-    }
-    Ok(())
-}
-#[derive(Debug)]
-struct Todo {
-    id: u16,
-    todo_text: String,
-    priority: String,
-}
-
-impl Display for Todo {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "○ {} \t\tID: {} \n  ->priority: {}\n",
-            self.todo_text, self.id, self.priority
-        )
-    }
-}
-
 fn main() {
-    let conn = create_table().expect("Failed to connect to DB");
+    let conn = create_table().expect("Failed to connect to the DB");
     let args = Args::parse();
     match args.operation {
         Operation::Add => {
-            insert_todo(conn, args.text, args.priority);
+            match insert_todo(conn, args.text, args.priority) {
+                Ok(()) => (),
+                Err(err) => println!("Problems with adding the todo {}", err),
+            };
         }
         Operation::Remove => {
             match remove_todo(conn, args.id.unwrap()) {
@@ -110,10 +66,16 @@ fn main() {
             };
         }
         Operation::Modify => {
-            modify_todo(conn, args.text, args.priority);
+            match modify_todo(conn) {
+                Ok(()) => (),
+                Err(err) => println!("{err}"),
+            };
         }
         Operation::Ls => {
-            get_all_todos(conn);
+            match get_all_todos(conn) {
+                Ok(()) => (),
+                Err(err) => println!("{err}"),
+            };
         }
     }
 }
